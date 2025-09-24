c2ce7754c42613473fd29c9bb99d33258555cfba
Deep1O1 <basusubhadeep37@gmail.com>
2025-09-25 01:22:51 +0530

    Modified mmt

--- src/test/java/MakeMyTrip/BaseTestMMT.java
+++ src/test/java/MakeMyTrip/BaseTestMMT.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import DriverManager.WebDriverInstanceSetup;
